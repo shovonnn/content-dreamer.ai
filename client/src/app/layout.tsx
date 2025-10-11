fd372d216e4d6b6ba3b1ec5cc27865c2e11c3c8c
Mahfuz Khan <mahfuzkhanshovon@gmail.com>
2025-10-11 16:55:16 +0600

    add gtm

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/Navbar";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,7 @@ export default function RootLayout({
         <Navbar />
         {children}
       </body>
+      <GoogleTagManager gtmId="GTM-5JNG4RVP" />
     </html>
   );
 }
